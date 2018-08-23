e443c01ffe179aa06e72195c5b83a27982782d73
Gabriele Pieretti <gabriele.pieretti@gmail.com>
2018-08-23 17:49:58 +0200

    add https to nginx

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+#letsencrypt certonly --webroot -w /var/www/letsencrypt -d $CN --agree-tos --email $EMAIL --non-interactive --text
+certbot certonly --manual -d *.yello.stiip.it --email supporto@stiip.it --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem
+# cp /etc/letsencrypt/archive/$CN/cert1.pem /var/certs/cert1.pem
+# cp /etc/letsencrypt/archive/$CN/privkey1.pem /var/certs/privkey1.pem
