080fedbc1e17a49c9d346d3678a8c5554c8cabb5
Gabriele Pieretti <gabriele.pieretti@gmail.com>
2019-01-24 17:20:24 +0100

    add command to renew certbot

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #letsencrypt certonly --webroot -w /var/www/letsencrypt -d $CN --agree-tos --email $EMAIL --non-interactive --text
-certbot certonly --manual -d *.yello.stiip.it --email supporto@stiip.it --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+certbot certonly --manual -d *.backyello.stiip.it --email supporto@stiip.it --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+certbot certonly --manual -d yello.stiip.it --email supporto@stiip.it --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
 
 # cp /etc/letsencrypt/archive/$CN/cert1.pem /var/certs/cert1.pem
 # cp /etc/letsencrypt/archive/$CN/privkey1.pem /var/certs/privkey1.pem
